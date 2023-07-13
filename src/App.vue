60878efdea59308877a4a6a6fa65ee7a017fc7bb
lorenz2396 <lorenzo.favaretto1996@gmail.com>
2023-07-13 16:55:27 +0200

    e

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default {
 </script>
 
 <template>
+  <!-- Utilizzo componenti -->
   <AppTitle/>
   <div>
     
@@ -25,16 +26,5 @@ export default {
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
