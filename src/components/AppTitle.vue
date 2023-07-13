f2f96d9b168780f9408828027d0670ecce7e4259
lorenz2396 <lorenzo.favaretto1996@gmail.com>
2023-07-13 16:46:32 +0200

    Hello World Vite

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        data() {
+            return{
+                title : 'La mia prima app con Vue'
+            }
+        }
+    }
+
+</script>
+
+<template>
+
+  <h1> {{ title }} </h1>
+
+</template>
+
+<style scoped>
+    h1{
+        text-align: center;
+        padding: 22px;
+    }
+</style>
+
